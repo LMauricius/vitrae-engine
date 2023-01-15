a72f035ca6401f8bc4354c585e2ce36b3633f2f3
LMauricius <mauriciosmit100@gmail.com>
2023-01-16 00:08:02 +0100

    Tabs

--- VitraeEngine/src/Resources/ShaderSteps/Group.cpp
+++ VitraeEngine/src/Resources/ShaderSteps/Group.cpp
@@ -3,45 +3,44 @@
 namespace Vitrae
 {
 
-        GroupShaderStep::GroupShaderStep()
-        {
+    GroupShaderStep::GroupShaderStep()
+    {
 
+    }
+
+    GroupShaderStep::~GroupShaderStep()
+    {
+
+    }
+
+    void GroupShaderStep::extractInputPropertyNames(std::set<String> &outNames) const
+    {
+        for (auto &step : stepList) {
+            step->extractInputPropertyNames(outNames);
         }
-
-        GroupShaderStep::~GroupShaderStep()
-        {
-
+    }
+    
+    void GroupShaderStep::extractInputVariableNames(std::set<String> &outNames) const
+    {
+        for (auto &step : stepList) {
+            step->extractInputVariableNames(outNames);
         }
-
-        void GroupShaderStep::extractInputPropertyNames(std::set<String> &outNames) const
-        {
+    }
+    
+    void GroupShaderStep::extractOutputVariableNames(std::set<String> &outNames) const
+    {
+        for (auto &step : stepList) {
+            step->extractOutputVariableNames(outNames);
+        }
+    }
+    
+    void GroupShaderStep::extractSourceShaderSteps(std::vector<SourceShaderStep*> &outSteps, const std::map<String, ShaderProperty> &properties)
+    {
+        if (!enablingCondition || enablingCondition(properties)) {
             for (auto &step : stepList) {
-                step->extractInputPropertyNames(outNames);
+                step->extractSourceShaderSteps(outSteps, properties);
             }
         }
-        
-        void GroupShaderStep::extractInputVariableNames(std::set<String> &outNames) const
-        {
-            for (auto &step : stepList) {
-                step->extractInputVariableNames(outNames);
-            }
-        }
-        
-        void GroupShaderStep::extractOutputVariableNames(std::set<String> &outNames) const
-        {
-            for (auto &step : stepList) {
-                step->extractOutputVariableNames(outNames);
-            }
-        }
-        
-        void GroupShaderStep::extractSourceShaderSteps(std::vector<SourceShaderStep*> &outSteps, const std::map<String, ShaderProperty> &properties)
-        {
-            if (!enablingCondition || enablingCondition(properties)) {
-                for (auto &step : stepList) {
-                    step->extractSourceShaderSteps(outSteps, properties);
-                }
-            }
-        }
-        
+    } 
 
 }
--- VitraeEngine/src/Resources/ShaderSteps/Switch.cpp
+++ VitraeEngine/src/Resources/ShaderSteps/Switch.cpp
@@ -3,58 +3,57 @@
 namespace Vitrae
 {
 
-        SwitchShaderStep::SwitchShaderStep()
-        {
+    SwitchShaderStep::SwitchShaderStep()
+    {
 
-        }
+    }
 
-        SwitchShaderStep::~SwitchShaderStep()
-        {
+    SwitchShaderStep::~SwitchShaderStep()
+    {
 
-        }
+    }
 
-        void SwitchShaderStep::extractInputPropertyNames(std::set<String> &outNames) const
-        {
-            for (auto &c : cases) {
-                c.choice->extractInputPropertyNames(outNames);
-            }
-            if (defaultChoice.has_value()) {
-                defaultChoice.value()->extractInputPropertyNames(outNames);
+    void SwitchShaderStep::extractInputPropertyNames(std::set<String> &outNames) const
+    {
+        for (auto &c : cases) {
+            c.choice->extractInputPropertyNames(outNames);
+        }
+        if (defaultChoice.has_value()) {
+            defaultChoice.value()->extractInputPropertyNames(outNames);
+        }
+    }
+    
+    void SwitchShaderStep::extractInputVariableNames(std::set<String> &outNames) const
+    {
+        for (auto &c : cases) {
+            c.choice->extractInputVariableNames(outNames);
+        }
+        if (defaultChoice.has_value()) {
+            defaultChoice.value()->extractInputVariableNames(outNames);
+        }
+    }
+    
+    void SwitchShaderStep::extractOutputVariableNames(std::set<String> &outNames) const
+    {
+        for (auto &c : cases) {
+            c.choice->extractOutputVariableNames(outNames);
+        }
+        if (defaultChoice.has_value()) {
+            defaultChoice.value()->extractOutputVariableNames(outNames);
+        }
+    }
+    
+    void SwitchShaderStep::extractSourceShaderSteps(std::vector<SourceShaderStep*> &outSteps, const std::map<String, ShaderProperty> &properties)
+    {
+        for (auto &c : cases) {
+            if (!c.enablingCondition || c.enablingCondition(properties)) {
+                c.choice->extractSourceShaderSteps(outSteps, properties);
+                return;
             }
         }
-        
-        void SwitchShaderStep::extractInputVariableNames(std::set<String> &outNames) const
-        {
-            for (auto &c : cases) {
-                c.choice->extractInputVariableNames(outNames);
-            }
-            if (defaultChoice.has_value()) {
-                defaultChoice.value()->extractInputVariableNames(outNames);
-            }
+        if (defaultChoice.has_value()) {
+            defaultChoice.value()->extractSourceShaderSteps(outSteps, properties);
         }
-        
-        void SwitchShaderStep::extractOutputVariableNames(std::set<String> &outNames) const
-        {
-            for (auto &c : cases) {
-                c.choice->extractOutputVariableNames(outNames);
-            }
-            if (defaultChoice.has_value()) {
-                defaultChoice.value()->extractOutputVariableNames(outNames);
-            }
-        }
-        
-        void SwitchShaderStep::extractSourceShaderSteps(std::vector<SourceShaderStep*> &outSteps, const std::map<String, ShaderProperty> &properties)
-        {
-            for (auto &c : cases) {
-                if (!c.enablingCondition || c.enablingCondition(properties)) {
-                    c.choice->extractSourceShaderSteps(outSteps, properties);
-                    return;
-                }
-            }
-            if (defaultChoice.has_value()) {
-                defaultChoice.value()->extractSourceShaderSteps(outSteps, properties);
-            }
-        }
-        
+    }   
 
 }
