977f1428cbaad31e6b40534a689a7faf2db26372
LMauricius <mauriciosmit100@gmail.com>
2023-01-19 22:30:51 +0100

    A Shader class

--- VitraeEngine/include/Vitrae/Resources/Shader.h
+++ VitraeEngine/include/Vitrae/Resources/Shader.h
@@ -1,2 +1,27 @@
 #pragma once
 
+#include "Vitrae/Util/Types.h"
+#include "Vitrae/ResourceManager.h"
+#include "Vitrae/Resources/ShaderStep.h"
+
+namespace Vitrae
+{
+
+    class Shader
+    {
+    public:
+        virtual ~Shader() = 0;
+
+        /**
+         * @brief Set the Output Step object
+         * 
+         * @param outputName The name of the output operation the step will be used for
+         * Could be "vertex", "geometry", "fragShadedColor", "fragDepthComponent", "fragDiffuseComponent", "fragSpecularComponent"...
+         * @param step The step that is used for the operation.
+         * The step or its substeps should not be modified while assigned to a Shader
+         */
+        virtual void setOutputStep(const String& outputName, casted_resource_ptr<ShaderStep> step) = 0;
+    };
+    
+}
+
