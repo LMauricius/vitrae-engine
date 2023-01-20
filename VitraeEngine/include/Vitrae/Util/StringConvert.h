9a307a7e712e1275444f2578b133ccd342c59375
LMauricius <mauriciosmit100@gmail.com>
2023-01-20 04:33:17 +0100

    Fixed bad concept usage

--- VitraeEngine/include/Vitrae/Util/StringConvert.h
+++ VitraeEngine/include/Vitrae/Util/StringConvert.h
@@ -12,7 +12,21 @@
 
 namespace Vitrae
 {
-    template<class T> requires requires(T t, std::stringstream ss) {ss << t;}
+    namespace StringConvertibleChooses
+    {
+        template<class T>
+        concept std_to_string = requires(T t) {std::to_string(t);};
+
+        template<class T>
+        concept glm_to_string = !std_to_string<T> && requires(T t) {glm::to_string(t);};
+
+        template<class T>
+        concept std_sstream = !glm_to_string<T> && requires(T t, std::stringstream ss) {ss << t;};
+
+    }
+    
+
+    template<class T> requires StringConvertibleChooses::std_sstream<T>
     String toString(const T& val)
     {
         std::stringstream ss;
@@ -20,13 +34,13 @@ namespace Vitrae
         return ss.str();
     }
 
-    template<class T> requires requires(T t) {glm::to_string(t);}
+    template<class T> requires StringConvertibleChooses::glm_to_string<T>
     String toString(const T& val)
     {
         return glm::to_string(val);
     }
 
-    template<class T> requires requires(T t) {std::to_string(t);}
+    template<class T> requires StringConvertibleChooses::std_to_string<T>
     String toString(const T& val)
     {
         return std::to_string(val);
@@ -40,5 +54,17 @@ namespace Vitrae
     String toString(const aiString& str)
     {
         return String(str.data, str.length);
+    }
+
+
+    template<class T> requires (!requires(T t) {toString(t);})
+    String toStringOrErr(const T& val)
+    {
+        return "<error in conversion>";
+    }
+    template<class T> requires requires(T t) {toString(t);}
+    String toStringOrErr(const T& val)
+    {
+        return toString(t);
     }
 }
