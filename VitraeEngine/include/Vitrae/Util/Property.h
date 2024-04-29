124e67c3c714c42f1f18342d746e9cbc2b40161e
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-04-30 01:42:31 +0200

    Added property helper files

--- VitraeEngine/include/Vitrae/Util/Property.h
+++ VitraeEngine/include/Vitrae/Util/Property.h
@@ -2,21 +2,10 @@
 
 #include "dynasma/pointer.hpp"
 
-#include <variant>
-#include "glm/glm.hpp"
+#include <any>
 
 namespace Vitrae
 {
-    class Texture;
-
-    using VariantProperty = std::variant<
-        int32_t,
-        glm::vec1, glm::vec2, glm::vec3, glm::vec4,
-        glm::mat2, glm::mat3, glm::mat4,
-        dynasma::LazyPtr<Texture>>;
-
-    struct VariantPropertySpec
-    {
-        VariantProperty exampleValue;
-    };
+    using Property = std::any;
+    using PropertySpec = std::type_info;
 }
---
+++ VitraeEngine/include/Vitrae/Util/StringId.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+namespace Vitrae
+{
+    /**
+     * A hash of a string, for quicker comparison and mapping
+     */
+    class StringId
+    {
+        std::size_t m_hash;
+
+    public:
+        StringId(const std::string &str);
+        StringId(const std::string_view str);
+        StringId(StringId &&) = default;
+        StringId(const StringId &) = default;
+
+        inline bool operator=(StringId id);
+
+        inline bool operator==(StringId id) const;
+        inline bool operator!=(StringId id) const;
+        inline bool operator>=(StringId id) const;
+        inline bool operator<=(StringId id) const;
+        inline bool operator>(StringId id) const;
+        inline bool operator<(StringId id) const;
+    }
+}
