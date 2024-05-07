830663a753dad1d044fc321c121a13593d8e7761
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-05-07 18:22:25 +0200

    Fixed issues with src file

--- VitraeEngine/src/Util/ScopedDict.cpp
+++ VitraeEngine/src/Util/ScopedDict.cpp
@@ -1,5 +1,7 @@
 #include "Vitrae/Util/ScopedDict.hpp"
 
+#include <stdexcept>
+
 namespace Vitrae
 {
 
@@ -10,17 +12,17 @@ ScopedDict::ScopedDict(const ScopedDict *parent) : m_parent{parent}
 {
 }
 
-void ScopedDict::set(StringID key, const Property &value)
+void ScopedDict::set(StringId key, const Property &value)
 {
     m_dict[key] = value;
 }
 
-void ScopedDict::set(StringID key, Property &&value)
+void ScopedDict::set(StringId key, Property &&value)
 {
     m_dict[key] = std::move(value);
 }
 
-const Property &ScopedDict::get(StringID key) const
+const Property &ScopedDict::get(StringId key) const
 {
     auto it = m_dict.find(key);
     if (it != m_dict.end())
@@ -32,7 +34,7 @@ const Property &ScopedDict::get(StringID key) const
     throw std::runtime_error{"Key not found: " + std::string(key)};
 }
 
-bool ScopedDict::has(StringID key) const
+bool ScopedDict::has(StringId key) const
 {
     return m_dict.find(key) != m_dict.end() || (m_parent && m_parent->has(key));
 }
