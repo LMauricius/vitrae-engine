5b1f8d7101fbe5e0964329d1dcada7d64e28ec76
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-05-16 11:46:08 +0200

    Renamed VectorInfo

--- VitraeEngine/include/Vitrae/TypeConversion/VectorTypeInfo.h
+++ VitraeEngine/include/Vitrae/TypeConversion/VectorTypeInfo.h
@@ -8,51 +8,51 @@
 namespace Vitrae
 {
 
-template <class VecT> struct VectorInfo;
+template <class VecT> struct VectorTypeInfo;
 
-template <> struct VectorInfo<float>
+template <> struct VectorTypeInfo<float>
 {
     using value_type = float;
     static constexpr std::size_t NumComponents = 1;
 };
 
-template <> struct VectorInfo<aiVector2D>
+template <> struct VectorTypeInfo<aiVector2D>
 {
     using value_type = ai_real;
     static constexpr std::size_t NumComponents = 2;
 };
-template <> struct VectorInfo<aiVector3D>
+template <> struct VectorTypeInfo<aiVector3D>
 {
     using value_type = ai_real;
     static constexpr std::size_t NumComponents = 3;
 };
-template <> struct VectorInfo<aiColor3D>
+template <> struct VectorTypeInfo<aiColor3D>
 {
     using value_type = ai_real;
     static constexpr std::size_t NumComponents = 3;
 };
-template <> struct VectorInfo<aiColor4D>
+template <> struct VectorTypeInfo<aiColor4D>
 {
     using value_type = ai_real;
     static constexpr std::size_t NumComponents = 4;
 };
 
-template <> struct VectorInfo<glm::vec1>
+template <> struct VectorTypeInfo<glm::vec1>
 {
     using value_type = float;
     static constexpr std::size_t NumComponents = 1;
 };
-template <> struct VectorInfo<glm::vec2>
+template <> struct VectorTypeInfo<glm::vec2>
 {
     using value_type = float;
     static constexpr std::size_t NumComponents = 2;
 };
-template <> struct VectorInfo<glm::vec3>
+template <> struct VectorTypeInfo<glm::vec3>
 {
     using value_type = float;
     static constexpr std::size_t NumComponents = 3;
 };
-template <> struct VectorInfo<glm::vec4>
+template <> struct VectorTypeInfo<glm::vec4>
 {
     using value_type = float;
     static constexpr std::size_t NumComponents = 4;
