8b953d6cc85a88dc5a64c0efcae474d4dcfa4daf
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-06-10 03:50:11 +0200

    Defined standard shader output names

--- VitraeEngine/include/Vitrae/Pipelines/Shading/Task.hpp
+++ VitraeEngine/include/Vitrae/Pipelines/Shading/Task.hpp
@@ -2,11 +2,24 @@
 
 #include "Vitrae/Pipelines/Task.hpp"
 
+#include "glm/glm.hpp"
+
 namespace Vitrae
 {
 class ShaderTask : public Task
 {
     using Task::Task;
 };
+
+namespace StandardShaderOutputNames
+{
+constexpr const char SURFACE_SHADER_OUTPUT[] = "fragment_shade";
+constexpr const char COMPUTE_SHADER_OUTPUT[] = "computed_data";
+} // namespace StandardShaderOutputNames
+
+namespace StandardShaderOutputTypes
+{
+constexpr const TypeInfo &SURFACE_SHADER_OUTPUT_TYPE = Variant::getTypeInfo<glm::vec4>();
+}
 
 } // namespace Vitrae
