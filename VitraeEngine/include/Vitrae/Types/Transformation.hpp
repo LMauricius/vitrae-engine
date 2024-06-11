16108aa3f9707dd13af0953e7ee0c3638691681b
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-06-11 02:03:24 +0200

    Fixed the Transformation

--- VitraeEngine/include/Vitrae/Types/Transformation.hpp
+++ VitraeEngine/include/Vitrae/Types/Transformation.hpp
@@ -6,40 +6,44 @@ namespace Vitrae
 {
 struct SimpleTransformation
 {
-    glm::vec3 pos;
-    glm::quat rot;
-    glm::vec3 scale;
+    glm::vec3 position;
+    glm::quat rotation;
+    glm::vec3 scaling;
 
-    inline float getPitch() const { return glm::pitch(rot); }
+    inline float getPitch() const { return glm::pitch(rotation); }
 
-    inline float getYaw() const { return glm::yaw(rot); }
+    inline float getYaw() const { return glm::yaw(rotation); }
 
-    inline float getRoll() const { return glm::roll(rot); }
+    inline float getRoll() const { return glm::roll(rotation); }
 
-    inline void setPitch(float pitch) { rot = glm::quat(glm::vec3(pitch, getYaw(), getRoll())); }
-
-    inline void setYaw(float yaw) { rot = glm::quat(glm::vec3(getPitch(), yaw, getRoll())); }
-
-    inline void setRoll(float roll) { rot = glm::quat(glm::vec3(getPitch(), getYaw(), roll)); }
-
-    glm::mat4 getModelMatrix() const
+    inline void setPitch(float pitch)
     {
-        return glm::translate(glm::mat4_cast(rot) * glm::scale(glm::mat4(1), scale), pos);
+        rotation = glm::quat(glm::vec3(pitch, getYaw(), getRoll()));
     }
 
-    glm::mat4 getViewMatrix() const { return glm::inverse(getModelMatrix()); }
+    inline void setYaw(float yaw) { rotation = glm::quat(glm::vec3(getPitch(), yaw, getRoll())); }
 
-    void move(glm::vec3 pos) { this->pos += pos; }
+    inline void setRoll(float roll) { rotation = glm::quat(glm::vec3(getPitch(), getYaw(), roll)); }
 
-    void rotate(glm::vec3 rotPiYaRo)
+    inline glm::mat4 getModelMatrix() const
     {
-        this->rot = glm::quat(glm::eulerAngles(this->rot) + rotPiYaRo);
+        return glm::translate(glm::mat4_cast(rotation) * glm::scale(glm::mat4(1), scaling),
+                              position);
     }
 
-    void scale(glm::vec3 scaling)
+    inline glm::mat4 getViewMatrix() const { return glm::inverse(getModelMatrix()); }
+
+    inline void move(glm::vec3 pos) { this->position += pos; }
+
+    inline void rotate(glm::vec3 rotPiYaRo)
     {
-        this->scale = glm::vec3(this->scale.x * scaling.x, this->scale.y * scaling.y,
-                                this->scale.z * scaling.z);
+        this->rotation = glm::quat(glm::eulerAngles(this->rotation) + rotPiYaRo);
+    }
+
+    inline void scale(glm::vec3 scaling)
+    {
+        this->scaling = glm::vec3(this->scaling.x * scaling.x, this->scaling.y * scaling.y,
+                                  this->scaling.z * scaling.z);
     }
 };
 } // namespace Vitrae
