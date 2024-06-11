ba4fb609765fe14dcb13d6c6fa58c660e2e96be1
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-06-11 02:03:33 +0200

    Implemented scene loading

--- VitraeEngine/include/Vitrae/Visuals/Scene.hpp
+++ VitraeEngine/include/Vitrae/Visuals/Scene.hpp
@@ -1,11 +1,67 @@
 #pragma once
 
+#include "Vitrae/Assets/Mesh.hpp"
+#include "Vitrae/Types/Transformation.hpp"
+
+#include "assimp/scene.h"
+#include "dynasma/keepers/abstract.hpp"
+#include "dynasma/managers/abstract.hpp"
+#include "dynasma/pointer.hpp"
+
+#include <filesystem>
+
 namespace Vitrae
 {
+class ComponentRoot;
 
-class Scene
+class Camera : public SimpleTransformation
 {
   public:
+    float zNear = 1.0, zFar = 1000.0;
+    float width, height;
+    float fov = 45.0;
+
+    inline glm::mat4 getPerspectiveMatrix() const
+    {
+        return glm::perspective(glm::radians(fov), width / height, zNear, zFar);
+    }
+};
+
+struct MeshProp
+{
+    dynasma::FirmPtr<Mesh> p_mesh;
+    SimpleTransformation transform;
+};
+
+/**
+ * Represents a scene
+ */
+class Scene : public dynasma::PolymorphicBase
+{
+  public:
+    struct AssimpLoadParams
+    {
+        const aiScene *p_extScene;
+        ComponentRoot &root;
+    };
+    struct FileLoadParams
+    {
+        std::filesystem::path filepath;
+        ComponentRoot &root;
+    };
+
+    Scene(const AssimpLoadParams &params);
+    Scene(const FileLoadParams &params);
+
+    /*
+    Scene parts (to be replaced with a more modular approach)
+    */
+
+    std::vector<MeshProp> meshes;
+    Camera camera;
+
+  protected:
+    void loadFromAssimp(const AssimpLoadParams &params);
 };
 
 } // namespace Vitrae
---
+++ VitraeEngine/src/Visuals/Scene.cpp
@@ -0,0 +1,61 @@
+#include "Vitrae/Visuals/Scene.hpp"
+
+#include "Vitrae/ComponentRoot.hpp"
+
+#include "assimp/Importer.hpp"
+#include "assimp/postprocess.h"
+#include "assimp/scene.h"
+
+namespace Vitrae
+{
+Scene::Scene(const AssimpLoadParams &params)
+{
+    loadFromAssimp(params);
+}
+
+Scene::Scene(const FileLoadParams &params)
+{
+    Assimp::Importer importer;
+
+    const aiScene *scene = importer.ReadFile(params.filepath.c_str(),
+                                             aiProcess_CalcTangentSpace | aiProcess_Triangulate |
+                                                 aiProcess_JoinIdenticalVertices |
+                                                 aiProcess_SortByPType | aiProcess_FlipUVs);
+
+    if (!scene) {
+        params.root.getErrStream() << importer.GetErrorString();
+        return;
+    }
+
+    loadFromAssimp({scene, params.root});
+
+    importer.FreeScene();
+}
+
+void Scene::loadFromAssimp(const AssimpLoadParams &params)
+{
+    MeshKeeper &meshKeeper = params.root.getComponent<MeshKeeper>();
+
+    if (params.p_extScene->HasMeshes()) {
+        for (unsigned int i = 0; i < params.p_extScene->mNumMeshes; ++i) {
+            dynasma::FirmPtr<Mesh> p_mesh = meshKeeper.new_asset(
+                {Mesh::AssimpLoadParams{params.p_extScene->mMeshes[i], params.root}});
+
+            meshes.emplace_back(MeshProp{p_mesh, SimpleTransformation()});
+        }
+    }
+
+    if (params.p_extScene->HasCameras()) {
+        const aiCamera *p_ext_camera = params.p_extScene->mCameras[0];
+
+        camera.position = glm::vec3(p_ext_camera->mPosition.x, p_ext_camera->mPosition.y,
+                                    p_ext_camera->mPosition.z);
+        camera.rotation = glm::lookAt(camera.position,
+                                      camera.position + glm::vec3(p_ext_camera->mLookAt.x,
+                                                                  p_ext_camera->mLookAt.y,
+                                                                  p_ext_camera->mLookAt.z),
+                                      glm::vec3(0, 1, 0));
+    }
+}
+
+} // namespace Vitrae
