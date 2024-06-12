a382f63bf1c102a7f49994ad6bba1bc344b1c5a3
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-06-12 21:08:55 +0200

    Added the Compositor!

--- VitraeEngine/include/Vitrae/Visuals/Compositor.hpp
+++ VitraeEngine/include/Vitrae/Visuals/Compositor.hpp
@@ -1,1 +1,40 @@
 #pragma once
+
+#include "Vitrae/Pipelines/Compositing/Task.hpp"
+#include "Vitrae/Pipelines/Pipeline.hpp"
+#include "Vitrae/Visuals/Scene.hpp"
+
+#include "dynasma/keepers/abstract.hpp"
+#include "dynasma/managers/abstract.hpp"
+#include "dynasma/pointer.hpp"
+
+namespace Vitrae
+{
+class FrameStore;
+class ComponentRoot;
+
+class Compositor : public dynasma::PolymorphicBase
+{
+  public:
+    Compositor(ComponentRoot &root, dynasma::FirmPtr<Method<ComposeTask>> p_method,
+               dynasma::FirmPtr<FrameStore> p_output);
+    virtual ~Compositor() = default;
+
+    void setComposeMethod(dynasma::FirmPtr<Method<ComposeTask>> p_method);
+    void setDefaultShadingMethod(dynasma::FirmPtr<Method<ShaderTask>> p_vertexMethod,
+                                 dynasma::FirmPtr<Method<ShaderTask>> p_fragmentMethod);
+    void setOutput(dynasma::FirmPtr<FrameStore> p_store);
+
+    void compose() const;
+
+    ScopedDict parameters;
+
+  protected:
+    ComponentRoot &m_root;
+    Pipeline<ComposeTask> m_pipeline;
+    std::map<StringId, dynasma::FirmPtr<FrameStore>> m_preparedFrameStores;
+    std::map<StringId, dynasma::FirmPtr<Texture>> m_preparedTextures;
+    dynasma::FirmPtr<Method<ShaderTask>> m_defaultVertexMethod, m_defaultFragmentMethod;
+};
+
+} // namespace Vitrae
---
+++ VitraeEngine/src/Visuals/Compositor.cpp
@@ -0,0 +1,65 @@
+#include "Vitrae/Visuals/Compositor.hpp"
+#include "Vitrae/ComponentRoot.hpp"
+
+#include <ranges>
+
+namespace Vitrae
+{
+class Renderer;
+
+Compositor::Compositor(ComponentRoot &root, dynasma::FirmPtr<Method<ComposeTask>> p_method,
+                       dynasma::FirmPtr<FrameStore> p_output)
+    : m_root(root), m_pipeline(p_method, {})
+{
+    m_preparedFrameStores[StandardCompositorOutputNames::OUTPUT] = p_output;
+
+    // here we actually setup the pipeline
+    setComposeMethod(p_method);
+}
+
+void Compositor::setComposeMethod(dynasma::FirmPtr<Method<ComposeTask>> p_method)
+{
+    m_pipeline = Pipeline<ComposeTask>(
+        p_method, {{PropertySpec{.name = StandardCompositorOutputNames::OUTPUT,
+                                 .typeInfo = StandardCompositorOutputTypes::OUTPUT_TYPE}}});
+
+    // clear the buffers (except for the final output)
+    auto display_frame = m_preparedFrameStores.at(StandardCompositorOutputNames::OUTPUT);
+    m_preparedFrameStores.clear();
+    m_preparedTextures.clear();
+    m_preparedFrameStores[StandardCompositorOutputNames::OUTPUT] = display_frame;
+
+    // fill the buffers
+    for (auto &pipeitem : std::ranges::reverse_view{m_pipeline.items}) {
+        pipeitem.p_task->prepareRequiredLocalAssets(m_preparedFrameStores, m_preparedTextures);
+    }
+}
+
+void Compositor::setDefaultShadingMethod(dynasma::FirmPtr<Method<ShaderTask>> p_vertexMethod,
+                                         dynasma::FirmPtr<Method<ShaderTask>> p_fragmentMethod)
+{
+    m_defaultVertexMethod = p_vertexMethod;
+    m_defaultFragmentMethod = p_fragmentMethod;
+}
+
+void Compositor::setOutput(dynasma::FirmPtr<FrameStore> p_store)
+{
+    m_preparedFrameStores[StandardCompositorOutputNames::OUTPUT] = p_store;
+}
+
+void Compositor::compose() const
+{
+    ScopedDict localVars(&parameters);
+
+    Renderer &rend = m_root.getComponent<Renderer>();
+    RenderRunContext context{.properties = localVars,
+                             .renderer = rend,
+                             .preparedCompositorFrameStores = m_preparedFrameStores,
+                             .preparedCompositorTextures = m_preparedTextures};
+
+    for (auto &pipeitem : m_pipeline.items) {
+        pipeitem.p_task->run(context);
+    }
+}
+
+} // namespace Vitrae
