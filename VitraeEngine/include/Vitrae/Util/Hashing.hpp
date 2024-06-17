8b2ff39fab393612cad87cd6e9c3ef614555c2a4
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-06-17 15:05:55 +0200

    Changed hashing for single hashes

--- VitraeEngine/include/Vitrae/Util/Hashing.hpp
+++ VitraeEngine/include/Vitrae/Util/Hashing.hpp
@@ -8,9 +8,9 @@ namespace Vitrae
 
 template <std::size_t N> inline std::size_t combinedHashes(std::span<const std::size_t, N> hashes)
 {
-    std::size_t seed = 0;
+    std::size_t seed = hashes[0];
 
-    for (std::size_t hash : *hashes) {
+    for (std::size_t hash : hashes.subspan(1)) {
         seed ^= hash + 0x9e3779b9 + (seed << 6) + (seed >> 2);
     }
 
