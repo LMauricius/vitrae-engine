9631fb4bf08aec65a11fa510fb32e0569a8d0a13
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2024-06-18 15:49:55 +0200

    InitializerList for PropertyList

--- VitraeEngine/include/Vitrae/Util/PropertyList.hpp
+++ VitraeEngine/include/Vitrae/Util/PropertyList.hpp
@@ -34,6 +34,22 @@ class PropertyList : public dynasma::PolymorphicBase
     PropertyList(PropertyList const &) = default;
     PropertyList(PropertyList &&) = default;
 
+    inline PropertyList(std::initializer_list<const PropertySpec> specs)
+    {
+        for (const auto &spec : specs) {
+            m_mappedSpecs.emplace(spec.name, spec);
+        }
+
+        m_hash = 0;
+        for (auto [nameId, spec] : m_mappedSpecs) {
+            m_specNameIds.push_back(nameId);
+            m_specList.push_back(spec);
+
+            m_hash = combinedHashes<3>(
+                {{m_hash, std::hash<StringId>{}(nameId), spec.typeInfo.p_id->hash_code()}});
+        }
+    }
+
     template <class ContainerT>
     PropertyList(const ContainerT &specs)
         requires(std::ranges::range<ContainerT> &&
